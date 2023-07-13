d4a6e6daf58cd52444bbc6a75297ef6c87828520
Lino Sanfilippo <l.sanfilippo@kunbus.com>
2023-07-13 07:12:58 +0200

    serial: core: Do not hold the port lock when setting the term GPIO
    Both callers of uart_rs485_config() call the function with the port lock
    held. uart_rs485_config() however may sleep so in both cases the call
    results in a warning splat.
    
    Fix this by moving the sleepable part (i.e. the call to
    uart_set_rs485_termination()) out of uart_rs485_config() into the callers.
    
    Fixes: 7c7f9bc986e6 ("serial: Deassert Transmit Enable on probe in driver-specific way")
    Signed-off-by: Lino Sanfilippo <l.sanfilippo@kunbus.com>

--- drivers/tty/serial/serial_core.c
+++ drivers/tty/serial/serial_core.c
@@ -1413,7 +1413,6 @@ static int uart_rs485_config(struct uart_port *port)
 	int ret;
 
 	uart_sanitize_serial_rs485(port, rs485);
-	uart_set_rs485_termination(port, rs485);
 
 	ret = port->rs485_config(port, NULL, rs485);
 	if (ret)
@@ -2480,6 +2479,9 @@ int uart_resume_port(struct uart_driver *drv, struct uart_port *uport)
 			if (ret == 0) {
 				if (tty)
 					uart_change_line_settings(tty, state, NULL);
+
+				uart_set_rs485_termination(uport,
+							   &uport->rs485);
 				spin_lock_irq(&uport->lock);
 				if (!(uport->rs485.flags & SER_RS485_ENABLED))
 					ops->set_mctrl(uport, uport->mctrl);
@@ -2583,6 +2585,7 @@ uart_configure_port(struct uart_driver *drv, struct uart_state *state,
 
 		/* Power up port for set_mctrl() */
 		uart_change_pm(state, UART_PM_STATE_ON);
+		uart_set_rs485_termination(port, &port->rs485);
 
 		/*
 		 * Ensure that the modem control lines are de-activated.
